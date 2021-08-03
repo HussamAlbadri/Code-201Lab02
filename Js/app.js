5ef6c5558993b9aae8db1a6fd1543f8e0e8e7f56
Hussam Albadri <hussamalbadri2@gmail.com>
2021-08-03 15:34:38 +0300

    FixedFile

--- Js/app.js
+++ Js/app.js
@@ -5,70 +5,66 @@ let name = prompt("What is your name ?")
 
 alert("Welcome to my protofilo...  " + name);
 alert("Hint for you: My birthday is near the end of the year (:")
-let firstQuestion = prompt("Do you know my Birth Date ?")
+let firstQuestion = prompt("Do you think that my birth date in December ?")
 
 if (firstQuestion != null) {
     firstQuestion = firstQuestion.toLowerCase();
 }
 
 switch (firstQuestion) {
-    case 'december':
+    case 'yes':
+    case 'y':
         alert('You\'r right')
         console.log('You\'r right');
         counter++;
         break;
-    case 'november':
-    case 'october':
-        alert('Almost Arrive *_*')
-        console.log('Almost Arrive *_*');
+    case 'no':
+    case 'n':
+        alert('Wrong answer')
+        console.log('Wrong answer');
         break;
-
-    default:
-        alert('Do i have a birthdate ! 0_0')
-        console.log('Do i have a birthdate ! 0_0');
 }
 
-let secondQuestion = prompt("What is my favourite hobby ?      choose one of these{ Football , Coding , Reading books , watch movies}")
+let secondQuestion = prompt("Do you that my favourite hobby is Football ?")
 
 if (secondQuestion != null) {
     secondQuestion = secondQuestion.toLowerCase();
 }
 switch (secondQuestion) {
-    case 'football':
-    case 'watch movies':
+    case 'yes':
+    case 'y':
         alert("No, Not it is , try again")
         console.log('No, Not it is , try again');
         break;
 
-    case 'coding':
-        alert("Yes, That's right (:")
-        console.log('yes, That\'s write (:');
-        counter++;
-        break;
-    case 'reading books':
-        alert("Yep, i like to read books too")
-        console.log('Yep, i like to read books too');
+    case 'no':
+    case 'n':
+        alert("Yes, That's right (: ")
+        console.log('yes, That\'s write (: ');
         counter++;
         break;
 }
 
-let thirdQuestion = prompt("What is my favourite color ?            choose one of these{ Black , White , Pink , Blue}")
+let thirdQuestion = prompt("Is Black consedierd as my favourite color ?")
 
 if (thirdQuestion != null) {
     thirdQuestion = thirdQuestion.toLowerCase();
 }
 switch (thirdQuestion) {
-    case 'black':
-        alert("Nice Choice")
-        console.log('Nice Choice');
+    case 'yes':
+    case 'y':
+        alert("Nice Choice, Perfect")
+        console.log('Nice Choice, Perfect');
         counter++;
         break;
 
-    case 'white':
-    case 'pink':
-    case 'blue':
+    case 'no':
+    case 'n':
         alert("Try later...GG")
         console.log('Try later...GG');
         break;
+    default:
+        alert("Hi there");
+
 }
 alert("You have " + counter + "  right answers")
