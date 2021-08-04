392c4741769747d124714c34cd0761097ee69a38
AliMwas <alimwas61@gmail.com>
2021-08-04 14:59:02 +0300

    add function

--- Js/app.js
+++ Js/app.js
@@ -2,15 +2,16 @@
 let counter = 0;
 let name = prompt("What is your name ?")
 
-alert("Welcome to my protofilo...  " + name);
-alert("Hint for you: My birthday is near the end of the year (:")
+ alert("Welcome to my protofilo...  " + name);
+ alert("Hint for you: My birthday is near the end of the year (:")
 
-// Question 1
-let firstQuestion = prompt("Do you think that my birth date in December ?")
+ // Question 1
+ function Question1(){
+     let firstQuestion = prompt("Do you think that my birth date in December ?")
 
-if (firstQuestion != null) {
-    firstQuestion = firstQuestion.toLowerCase();
-}
+ if (firstQuestion != null) {
+     firstQuestion = firstQuestion.toLowerCase();
+ }
 
 switch (firstQuestion) {
     case 'yes':
@@ -25,8 +26,14 @@ switch (firstQuestion) {
         console.log('Wrong answer');
         break;
 }
-// Question 2
-let secondQuestion = prompt("Do you that my favourite hobby is Football ?")
+return firstQuestion;
+}
+Question1();
+
+
+//  Question 2
+function Question2(){
+    let secondQuestion = prompt("Do you that my favourite hobby is Football ?")
 
 if (secondQuestion != null) {
     secondQuestion = secondQuestion.toLowerCase();
@@ -45,29 +52,40 @@ switch (secondQuestion) {
         counter++;
         break;
 }
-// Question 3
-let thirdQuestion = prompt("Is Black consedierd as my favourite color ?")
-
-if (thirdQuestion != null) {
-    thirdQuestion = thirdQuestion.toLowerCase();
+return secondQuestion;
 }
-switch (thirdQuestion) {
-    case 'yes':
-    case 'y':
-        alert("Nice Choice, Perfect")
-        console.log('Nice Choice, Perfect');
-        counter++;
-        break;
+Question2();
 
-    case 'no':
-    case 'n':
-        alert("Try later...GG")
-        console.log('Try later...GG');
-        break;
+
+//  Question 3
+function Question3(){
+    let thirdQuestion = prompt("Is Black consedierd as my favourite color ?")
+
+    if (thirdQuestion != null) {
+        thirdQuestion = thirdQuestion.toLowerCase();
+    }
+    switch (thirdQuestion) {
+        case 'yes':
+        case 'y':
+            alert("Nice Choice, Perfect")
+            console.log('Nice Choice, Perfect');
+            counter++;
+            break;
+    
+        case 'no':
+        case 'n':
+            alert("Try later...GG")
+            console.log('Try later...GG');
+            break;
+    }
+    return thirdQuestion;
 }
+Question3();
 
 // Ouestion 4
-let fourthQuestion = prompt("Is Winter is my favourite season ?")
+function Question4(){
+
+    let fourthQuestion = prompt("Is Winter is my favourite season ?")
 if (fourthQuestion != null) {
     fourthQuestion = fourthQuestion.toLowerCase();
 }
@@ -87,9 +105,16 @@ switch (fourthQuestion) {
         break;
 }
 
+return fourthQuestion;
+}
+Question4();
 
-//Question 5
-let favCar = ['mercedes', 'lexus', '1967 mustang', 'kia', ]
+
+
+// //Question 5
+function Question5(){
+
+    let favCar = ['mercedes', 'lexus', '1967 mustang', 'kia', ]
 let fifthQuestion = prompt("Guess my favourite car.. \nPick one of these brands : (Mercedes , BMW , Ferrari , Toyota , Lexus , Kia , Mitsubishi , Pegouet , 1967 Mustang")
 
 fifthQuestion = fifthQuestion.toLowerCase();
@@ -111,31 +136,54 @@ for (let i = 0; i < 4; i++) {
     }
 }
 
-//Question 6
-for (let i = 0; i < 4; i++) {
-
-    let number = prompt('Guess my favorite number  ' + name);
-    if (number > 33) {
-        alert('You guessed too high! Guess again  ' + name);
-
-    } else if (number < 33) {
-        alert('You guessed too low! Guess again  ' + name);
-    } else if (number == 33) {
-        alert('That is correct ' + name + ' My favorite number is 33!');
-        counter++;
-        break;
-    }
+return fifthQuestion;
 }
-alert("Yor ran out of guesses, the right answer is (33)")
+Question5();
+
+
+//Question 6
+function Question6 (){
+
+    let i = 0
+    for (i = 0; i < 4; i++) {
+
+        let number = prompt('Guess my favorite number  ' + name);
+        if (number > 33) {
+            alert('You guessed too high! Guess again  ' + name);
+    
+        } else if (number < 33) {
+            alert('You guessed too low! Guess again  ' + name);
+        } else if (number == 33) {
+            alert('That is correct ' + name + ' My favorite number is 33!');
+            counter++;
+            break;
+        
+        }
+        
+    }
+    if(i>3){
+        alert("Yor ran out of guesses, the right answer is (33)")
+}
+    
+    
 
 
 
+//return number;
+} 
+Question6();
 
-Question 7
+
+//Question 7
 let userPc = ['dell', 'mac', 'acer', 'lenovo']
 let userInput = prompt('Put your favourite pc brand...')
-
 userInput = userInput.toLowerCase();
+
+
+function Question7(){
+    
+
+
 
 let flagBreak = true;
 for (let i = 0; i < 6; i++) {
@@ -155,6 +203,11 @@ for (let i = 0; i < 6; i++) {
     }
 }
 alert("The pc's brands is... " + userPc)
+return userInput;
+
+}
+Question7();
+
 
 
 alert(name + " have " + counter + "  right answers out of 7")
